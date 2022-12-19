c4e08064c88a2a2615b05f5d6aed2e099f421c4d
Nad1m-A-A <nadim.alaa@hotmail.com>
2022-12-19 20:16:54 +0400

    final

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Head>
-        <title>Productivity Managment Zone</title>
+        <title>Productivity Management Zone</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet"/> 
